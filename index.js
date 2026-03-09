472bfc18cafd7d32fd2ee4415c5131576bcccc70
sanjaysriat08 <sanjaysriat@gmail.com>
2026-03-09 11:47:15 +0530

    Version 1 without localStorage the page

---
+++ index.js
@@ -0,0 +1,61 @@
+
+let todoList = [{name :'',dueDate:''}];
+
+function renderTodoList() {
+
+    let todoRenderHTML = '';
+
+    for(let i=0 ; i<todoList.length ; i++) {
+
+        const todoObject = todoList[i];
+        if(todoObject.name != '' && todoObject.dueDate!='') {
+            const name = todoObject.name;
+            const dueDate = todoObject.dueDate;
+            const html = 
+            `
+            <div>${dueDate}</div>
+            <div>${name}</div>
+                
+                <button class='deleteButton' onclick='
+                todoList.splice(${i},1);
+                renderTodoList();
+                '>Delete</button>`;
+            
+            todoRenderHTML += html;
+        }
+    }
+
+    document.querySelector('.todoRender').innerHTML = todoRenderHTML;
+
+}
+
+function insertTodo() {
+
+    let inputTodo = document.querySelector('.todoOne');
+    let todoDueDate = document.querySelector('.dueDateCalender');
+
+    let name = inputTodo.value;
+    let dueDate = todoDueDate.value;
+    
+    todoList.push(
+        {
+            name : name,
+            dueDate : dueDate
+        }
+    );
+
+    //console.log(todoList);
+    
+    inputTodo.value = '';
+    todoDueDate.value = '';
+
+    renderTodoList();
+}
+
+function enterToInsert(event) {
+
+    if(event.key === 'Enter') {
+
+        insertTodo();
+    }
+}
